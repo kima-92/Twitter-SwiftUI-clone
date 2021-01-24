d81ae8fcf3704ddc9bd81b34972f9aa40b52c1f9
Wilma <kima_92@fastmail.com>
2021-01-23 19:14:19 -0500

    Creating the project

---
+++ Twitter clone/Twitter clone/Twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_cloneApp.swift
+//  Twitter clone
+//
+//  Created by macbook on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
