7cdabdbe2f52891b1e48cbba95a27eb7e33e979f
Wilma <kima_92@fastmail.com>
2021-01-30 23:59:51 -0500

    Creating the ChatBubble

---
+++ Twitter clone/Views/Messages/ChatBubble.swift
@@ -0,0 +1,29 @@
+//
+//  ChatBubble.swift
+//  Twitter clone
+//
+//  Created by macbook on 1/29/21.
+//
+
+import SwiftUI
+
+struct ChatBubble: Shape {
+    
+    // MARK: - Properties
+    
+    var isFromCurrentUser: Bool
+    
+    // MARK: - Body
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight, isFromCurrentUser ? .bottomLeft: .bottomRight], cornerRadii: CGSize(width: 16, height: 16))
+        
+        return Path(path.cgPath)
+    }
+}
+
+struct ChatBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatBubble(isFromCurrentUser: false)
+    }
+}
