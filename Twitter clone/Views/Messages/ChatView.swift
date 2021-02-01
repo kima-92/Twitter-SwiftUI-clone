68c20f4abfcb7166ec1537267f355ea84c1847e4
Wilma <kima_92@fastmail.com>
2021-01-31 22:32:10 -0500

    Creating MockMessages to use while implementing the ChatBubbles in the ChatView

--- Twitter clone/Views/Messages/ChatView.swift
+++ Twitter clone/Views/Messages/ChatView.swift
@@ -19,21 +19,44 @@ struct ChatView: View {
         VStack {
             ScrollView {
                 VStack {
+                    
                     // Chat Bubbles
-                    ForEach(0..<15) { _ in
+                    ForEach(MOCK_MESSAGES) { message in
                         HStack {
-                            Spacer()
-                            Text("Test message text")
-                                .padding()
-                                .background(Color.blue)
-                                .clipShape(ChatBubble(isFromCurrentUser: true))
-                                .foregroundColor(.white)
-                                .padding(.horizontal)
+                            
+                            // Current user messages
+                            if message.isCurrentUser {
+                                Spacer()
+                                Text(message.messageText)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .clipShape(ChatBubble(isFromCurrentUser: true))
+                                    .foregroundColor(.white)
+                                    .padding(.horizontal)
+                                
+                            // Not current user messages
+                            } else {
+                                HStack(alignment: .bottom) {
+                                    Image(message.imageName)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 40, height: 40)
+                                        .clipShape(Circle())
+                                    
+                                    Text(message.messageText)
+                                        .padding()
+                                        .background(Color(.systemGray5))
+                                        .clipShape(ChatBubble(isFromCurrentUser: false))
+                                        .foregroundColor(.black)
+                                    Spacer() // <- Not in tutorial
+                                }.padding(.horizontal)
+                            }
                         }
                     }
                 }
             }
             MessageInputView(mesageText: $messageText)
+                .padding()
         }
     }
 }
