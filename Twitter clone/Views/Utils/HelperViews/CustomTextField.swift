9af4c85800a63bd8f6767a4d009e8285a3036756
Wilma <kima_92@fastmail.com>
2021-02-23 19:43:41 -0500

    Built the CustomTextField view. Which can hide a custom placeholder when text is inserted in the TextField.

---
+++ Twitter clone/Views/Utils/HelperViews/CustomTextField.swift
@@ -0,0 +1,44 @@
+//
+//  CustomTextField.swift
+//  Twitter clone
+//
+//  Created by macbook on 2/23/21.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    
+    // MARK: - Properties
+    
+    @Binding var text: String
+    let placeholder: Text
+    
+    // MARK: - Body
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if text.isEmpty {
+                placeholder
+                    .foregroundColor(Color(.init(white: 1, alpha: 0.87)))
+                    .padding(.leading, 40)
+            }
+            
+            HStack(spacing:16) {
+                Image(systemName: "envelope")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.white)
+                
+                TextField("", text: $text)
+            }
+        }
+    }
+}
+
+struct CustomTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTextField(text: .constant(""), placeholder: Text("Email"))
+    }
+}
