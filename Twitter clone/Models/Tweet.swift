ac2023399259e7b42fcb381990ab23eeb411cb98
Wilma <kima_92@fastmail.com>
2021-03-16 07:38:06 -0400

    Creating the Tweet model

---
+++ Twitter clone/Models/Tweet.swift
@@ -0,0 +1,30 @@
+//
+//  Tweet.swift
+//  Twitter clone
+//
+//  Created by macbook on 3/16/21.
+//
+
+import Firebase
+
+struct Tweet: Identifiable {
+    let id: String  // Actually NEEDED to confirm to the Identifiable protocol
+    let username: String
+    let profileImageUrl: String
+    let fullname: String
+    let caption: String
+    let likes: Int
+    let uid: String
+    let timestamp: Timestamp
+    
+    init(dictionary: [String: Any]) {
+        self.id = dictionary["uid"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImage"] as? String ?? ""
+        self.fullname = dictionary["fullname"] as? String ?? ""
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.likes = dictionary["likes"] as? Int ?? 0
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+    }
+}
