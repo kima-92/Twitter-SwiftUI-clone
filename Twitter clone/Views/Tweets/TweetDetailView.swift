dbf460cc070fcb31d72c24e9a1476abce729f2db
Wilma <kima_92@fastmail.com>
2021-03-19 20:56:55 -0400

    Built the UI for the TweetDetailView

--- Twitter clone/Views/Tweets/TweetDetailView.swift
+++ Twitter clone/Views/Tweets/TweetDetailView.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct TweetDetailView: View {
     var body: some View {
-        Text("Tweet Details View")
+        VStack(alignment: .leading, spacing: 16) {
+            
+            // Profile Image, name and username
+            HStack {
+                Image("batman")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(28)
+                
+                VStack(alignment: .leading, spacing:4) {
+                    Text("Bruce Wayne")
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text("@batman")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            // Tweet and Date
+            Text("It's not who I am underneath, but what I do that defines me")
+                .font(.system(size: 22))
+            
+            Text("7:22 PM • 05/01/2020")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
+            
+            Divider()
+            
+            // Retweets and Likes
+            HStack(spacing: 12) {
+                HStack {
+                    Text("0")
+                        .font(.system(size: 14))
+                    
+                    Text("Retweets")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+                HStack {
+                    Text("0")
+                        .font(.system(size: 14))
+                    
+                    Text("Likes")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+            }
+            Divider()
+            
+            // Bottom buttons
+            TweetActionsView()
+            Spacer()
+        }.padding()
     }
 }
 
