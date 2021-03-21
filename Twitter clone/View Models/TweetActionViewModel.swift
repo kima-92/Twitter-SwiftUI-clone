812bd4472dbcd88aa3d15f73b95a5d03b6009257
Wilma <kima_92@fastmail.com>
2021-03-21 00:26:32 -0400

    Implementing the unlikeTweet method. Building the checkIfUserLikedTweet and calling it when the TweetActionViewModel is initialized

--- Twitter clone/View Models/TweetActionViewModel.swift
+++ Twitter clone/View Models/TweetActionViewModel.swift
@@ -19,6 +19,7 @@ class TweetActionViewModel: ObservableObject {
     
     init(tweet: Tweet) {
         self.tweet = tweet
+        checkIfUserLikedTweet()
     }
     
     // MARK: - Methods
@@ -32,7 +33,7 @@ class TweetActionViewModel: ObservableObject {
         let userDocumentRef = COLLECTION_USERS.document(uid)
         
         // Updating only the likes property of this tweet
-        tweetDocumentRef.updateData(["likes": tweet.likes + 1])
+        tweetDocumentRef.updateData(["likes": tweet.likes + 1]) // TODO: Bug - This is causing a bug in Firebase for the likes propety
         
         // References to the "tweet-likes" and "user-likes" collections - Firebase
         let tweetLikesCollectionRef = tweetDocumentRef.collection("tweet-likes")
@@ -49,7 +50,44 @@ class TweetActionViewModel: ObservableObject {
     }
     
     func unlikeTweet() {
+        // Current user's ID
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
         
+        // Get the reference to both this tweet and the current user - Firebase
+        let tweetDocumentRef = COLLECTION_TWEETS.document(tweet.id)
+        let userDocumentRef = COLLECTION_USERS.document(uid)
+        
+        // Updating only the likes property of this tweet
+        tweetDocumentRef.updateData(["likes": tweet.likes - 1]) // TODO: Bug - This is causing a bug in Firebase for the likes propety
+        
+        // References to the "tweet-likes" and "user-likes" collections - Firebase
+        let tweetLikesCollectionRef = tweetDocumentRef.collection("tweet-likes")
+        let userLikesCollectionRef = userDocumentRef.collection("user-likes")
+        
+        // Delete the uid from inside this tweet's "tweet-likes" collection,
+        // Delete this tweet's id from inside the current user's "user-likes" collection
+        // Set didLike to false so we can update the UI
+        tweetLikesCollectionRef.document(uid).delete { _ in
+            userLikesCollectionRef.document(self.tweet.id).delete { _ in
+                self.didLike = false
+            }
+        }
+    }
+    
+    private func checkIfUserLikedTweet() {
+        // Current user's ID
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        
+        // Try to get the reference to this tweet from inside of the current user's "user-likes" collection - Firebase
+        let userDocumentRef = COLLECTION_USERS.document(uid)
+        let userLikesCollectionRef = userDocumentRef.collection("user-likes")
+        let tweetDocumentRef = userLikesCollectionRef.document(tweet.id)
+        
+        tweetDocumentRef.getDocument { documentSnapshot, error in
+            // If the tweet's ID exists in this collection, set didLike to true
+            guard let didLike = documentSnapshot?.exists else { return }
+            self.didLike = didLike
+        }
     }
 }
 
